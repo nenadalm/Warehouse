87519ec21048c6b15814caede2b97e5dea44bd63
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-12-06 23:06:19 +0100

    Formatting, fixed react warnings

--- src/warehouse/core.cljs
+++ src/warehouse/core.cljs
@@ -15,23 +15,24 @@
                                           {:type :success
                                            :message "Something good happened"}]}))
 
-(def index (.lunr js/window (fn []
-                     (this-as this
-                              (.field this "name")
-                              (.field this "tags")))))
+(defonce index (.lunr js/window (fn []
+                                  (this-as this
+                                           (.field this "name")
+                                           (.field this "tags")))))
 
 (defn update-index [index, ns]
   (doseq [[k component] (:components @ns)]
     (.update index (clj->js {:id (:id component)
-                          :name (:name component)
-                          :tags (:tags component)}))))
+                             :name (:name component)
+                             :tags (:tags component)}))))
 
 (defn- on-state-load [response]
   (reset! app-state (document->state response @app-state)))
 
 (storage/load-state on-state-load nil)
+
 (add-watch app-state :storeer (fn [k ns os]
-                          (storage/store-state (state->document @ns))))
+                                (storage/store-state (state->document @ns))))
 
 (defn form [item]
   [:div
@@ -41,20 +42,20 @@
              :value (:name @item)
              :on-change (fn [e]
                           (swap! item assoc :name (.-target.value e)))}]]
-  [:div
-   [:label "Tags: "
-    [:input {:name "tags"
-             :type "text"
-             :value (:tags @item)
-             :on-change (fn [e]
-                          (swap! item assoc :tags (.-target.value e)))}]]]
-  [:div
-   [:label "Amount: "
-    [:input {:name "amount"
-             :type "number"
-             :value (:amount @item)
-             :on-change (fn [e]
-                          (swap! item assoc :amount (.-target.value e)))}]]]])
+   [:div
+    [:label "Tags: "
+     [:input {:name "tags"
+              :type "text"
+              :value (:tags @item)
+              :on-change (fn [e]
+                           (swap! item assoc :tags (.-target.value e)))}]]]
+   [:div
+    [:label "Amount: "
+     [:input {:name "amount"
+              :type "number"
+              :value (:amount @item)
+              :on-change (fn [e]
+                           (swap! item assoc :amount (.-target.value e)))}]]]])
 
 (defn item [data k]
   (let [editing (atom false)]
@@ -89,7 +90,7 @@
   (if (clojure.string/blank? (get-in @app-state [:filter :val]))
     (:components @app-state)
     (loop [indexes (map #(get % "ref") (get-in @app-state [:filter :search]))
-          res []]
+           res []]
       (if (empty? indexes)
         res
         (recur (rest indexes) (conj res [(first indexes) (get (:components @app-state) (first indexes))]))))))
@@ -103,8 +104,8 @@
                :class "close"
                :on-click (fn [e]
                            (swap! app-state assoc :notifications (concat
-                                                                  (subvec (:notifications @app-state) 0 k)
-                                                                  (subvec (:notifications @app-state) (inc k)))))} "X"]]))
+                                                                   (subvec (:notifications @app-state) 0 k)
+                                                                   (subvec (:notifications @app-state) (inc k)))))} "X"]]))
 
 (defn notifications []
   (let [notifications (:notifications @app-state)]
@@ -125,23 +126,23 @@
                  :on-change (fn [e]
                               (swap! app-state assoc :filter {:val (.-target.value e)
                                                               :search (js->clj (.search index (.-target.value e)))}))}]]
-         (if (true? @adding)
-           [:form
-             [form new-item]
-             [:button {:type "button"
-                       :on-click (fn []
-                                   (let [k (if (empty? (:components @app-state))
-                                             1
-                                             (inc (apply max (keys (:components @app-state)))))]
-                                     (swap! app-state assoc-in [:components k] (assoc @new-item :id k :tags (string->array (:tags @new-item)))))
-                                   (reset! adding false))} "Save"]
-             [:button {:type "button" :on-click #(reset! adding false)} "Cancel"]]
-           [:button {:on-click (fn [e]
-                                 (reset! adding true))} "Add new"])
+       (if (true? @adding)
+         [:form
+          [form new-item]
+          [:button {:type "button"
+                    :on-click (fn []
+                                (let [k (if (empty? (:components @app-state))
+                                          1
+                                          (inc (apply max (keys (:components @app-state)))))]
+                                  (swap! app-state assoc-in [:components k] (assoc @new-item :id k :tags (string->array (:tags @new-item)))))
+                                (reset! adding false))} "Save"]
+          [:button {:type "button" :on-click #(reset! adding false)} "Cancel"]]
+         [:button {:on-click (fn [e]
+                               (reset! adding true))} "Add new"])
        [:ul {:class "components-list"}
-         (for [[k v] (get-visible-components)]
-           [:li {:class "component"}
-             ^{:key (:name v)} [item v k]])]])))
+        (for [[k v] (get-visible-components)]
+          ^{:key (:name v)} [:li {:class "component"}
+                             [item v k]])]])))
 
 (reagent/render-component [page]
                           (.getElementById js/document "app"))
@@ -152,11 +153,11 @@
                         :tags (:tags component)})))
 
 (add-watch app-state :indexer (fn [k ns os]
-                           (update-index index ns)))
+                                (update-index index ns)))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
 
