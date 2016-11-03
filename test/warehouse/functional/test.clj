ddffae23b68f12b546250c7d8b5bd8d2f3d582d0
Miloslav Nenadal <nenadalm@gmail.com>
2016-11-03 20:42:55 +0100

    Fix selenium tests

--- test/warehouse/functional/test.clj
+++ test/warehouse/functional/test.clj
@@ -3,45 +3,104 @@
         [clojure.test]))
 
 ;(System/setProperty "webdriver.chrome.driver" "/home/nenadalm/Downloads/selenium/chromedriver")
-;(System/setProperty "phantomjs.binary.path" "/opt/phantomjs/bin/phantomjs")
-
 ;(def browser-spec {:browser :chrome})
+;(implicit-wait 3000)
+
 (def browser-spec {:browser :phantomjs})
 
 (set-driver! browser-spec)
 (set-finder! xpath-finder)
+
+(def params {:base-url "http://localhost:3449"})
+
+(def fixtures {:components
+               [{:id 1
+                :name "EPR212A408000Z"
+                :tags ["optocoupler"]
+                :tags-string "optocoupler"
+                :expected-tags-string "optocoupler"
+                :amount 7}
+               {:id 2
+                :name "2N3904"
+                :tags ["transistor"]
+                :tags-string "transistor"
+                :expected-tags-string "transistor"
+                :amount 8}
+               {:id 3
+                :name "LF33CV"
+                :tags ["linear regulator"]
+                :tags-string "linear regulator"
+                :expected-tags-string "linear regulator"
+                :amount 10}
+               {:id 4
+                :name "BD241C"
+                :tags ["transistor"]
+                :tags-string "transistor"
+                :expected-tags-string "transistor"
+                :amount 4}
+               {:id 5
+                :name "HC49/US QM 16.000MHZ"
+                :tags ["crystal" "oscillator"]
+                :tags-string "crystal, oscillator"
+                :expected-tags-string "crystal, oscillator"
+                :amount 1}
+                {:id 6
+                 :name "AVRProg USB v3"
+                 :tags ["rs232", "serial", "usb"]
+                 :tags-string "rs232, serial, usb"
+                 :expected-tags-string "rs232, serial, usb"
+                 :amount 1}]})
 
 (defn clear [q]
   (send-keys q (clojure.string/join
                  ""
                  (repeat (count (value q)) (clj-webdriver.core/key-code :back_space)))))
 
-(deftest edit-form-with-save []
-  (to "http://localhost:3449")
-  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'EPR212A408000Z')]"))
-  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'optocoupler')]"))
-  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), '7')]"))
-  (click "//button[contains(text(), 'Edit')]")
-  (wait-until (present? "//input[@name]"))
-
-  (is (present? "//input[@name='name' and @value='EPR212A408000Z']"))
-  (is (present? "//input[@name='tags' and @value='optocoupler']"))
-  (is (present? "//input[@name='amount' and @value='7']"))
-
-  (clear "//input[@name='name']")
-  (clear "//input[@name='tags']")
+(defn create-component [component]
+  (click "//button[contains(text(), 'Add new')]")
+  (input-text "//input[@name='name']" (:name component))
+  (input-text "//input[@name='tags']" (:tags-string component))
+  ;todo: comment and check tests
   (clear "//input[@name='amount']")
-  (input-text "//input[@name='name']" "new name")
-  (input-text "//input[@name='tags']" "tag1, tag2")
-  (input-text "//input[@name='amount']" "20")
-  (click "//button[contains(text(), 'Save')]")
-  (wait-until (present? "//li[@class='component'][1]"))
+  (input-text "//input[@name='amount']" (str (:amount component)))
+  (click "//button[contains(text(), 'Save')]"))
 
-  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'new name')]"))
-  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'tag1, tag2')]"))
-  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), '20')]")))
+(defn component-value-selector [n v]
+  (str "//li[@class='component'][" n "]//span[@class='value' and contains(text(), '" v "')]"))
 
-(deftest edit-form-with-cancel []
+(deftest components []
+
+  ;;;;;;;;;;;;; Create components ;;;;;;;;;;;;;
+
+  ; reset env
+  (to (:base-url params))
+  (execute-script "localStorage.clear();")
+
+  (to (:base-url params))
+
+  ; guard: there is zero components
+  (is (empty? (elements "//li[@class='component']")))
+
+  (doseq [component (:components fixtures)]
+    (create-component component)
+
+    ; check values of created components
+    (is (present? (component-value-selector (:id component) (:name component))))
+    (is (present? (component-value-selector (:id component) (:expected-tags-string component))))
+    (is (present? (component-value-selector (:id component) (:amount component))))
+
+    ; check number of components
+    (is (=
+         (:id component)
+         (count (elements "//li[@class='component']")))))
+
+  ; check total of created components
+  (is (=
+       (count (:components fixtures))
+       (count (elements "//li[@class='component']"))))
+
+  ;;;;;;;;;;;;; Edit form with cancel ;;;;;;;;;;;;;
+
   (to "http://localhost:3449")
   (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'EPR212A408000Z')]"))
   (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'optocoupler')]"))
@@ -64,9 +123,36 @@
 
   (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'EPR212A408000Z')]"))
   (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'optocoupler')]"))
-  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), '7')]")))
+  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), '7')]"))
 
-(deftest search-component []
+  ;;;;;;;;;;;;; Edit form with save ;;;;;;;;;;;;;
+
+  (to "http://localhost:3449")
+  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'EPR212A408000Z')]"))
+  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'optocoupler')]"))
+  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), '7')]"))
+  (click "//button[contains(text(), 'Edit')]")
+  (wait-until (present? "//input[@name]"))
+
+  (is (present? "//input[@name='name' and @value='EPR212A408000Z']"))
+  (is (present? "//input[@name='tags' and @value='optocoupler']"))
+  (is (present? "//input[@name='amount' and @value='7']"))
+
+  (clear "//input[@name='name']")
+  (clear "//input[@name='tags']")
+  (clear "//input[@name='amount']")
+  (input-text "//input[@name='name']" "new name")
+  (input-text "//input[@name='tags']" "tag1, tag2")
+  (input-text "//input[@name='amount']" "20")
+  (click "//button[contains(text(), 'Save')]")
+  (wait-until (present? "//li[@class='component'][1]"))
+
+  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'new name')]"))
+  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'tag1, tag2')]"))
+  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), '20')]"))
+
+  ;;;;;;;;;;;;; Search component ;;;;;;;;;;;;;
+
   (to "http://localhost:3449")
 
   ; guard
@@ -81,9 +167,10 @@
   (clear "//input[@name='search']")
   (input-text "//input[@name='search']" "rs232")
   (is (= 1 (count (elements "//li[@class='component']"))))
-  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'AVRProg USB v3')]")))
+  (is (present? "//li[@class='component'][1]//span[@class='value' and contains(text(), 'AVRProg USB v3')]"))
 
-(deftest search-edited-component []
+  ;;;;;;;;;;;;; Search edited component ;;;;;;;;;;;;;
+
   (to "http://localhost:3449")
 
   ; guard
@@ -97,19 +184,20 @@
   (input-text "//input[@name='name']" "wtf")
   (click "//button[contains(text(), 'Save')]")
   (input-text "//input[@name='search']" "wtf")
-  (is (= 1 (count (elements "//li[@class='component']")))))
+  (is (= 1 (count (elements "//li[@class='component']"))))
 
-(deftest search-new-component []
+  ;;;;;;;;;;;;; Search new component ;;;;;;;;;;;;;
+
   (to "http://localhost:3449")
 
   ; guard
-  (input-text "//input[@name='search']" "wtf")
+  (input-text "//input[@name='search']" "new-component")
   (is (= 0 (count (elements "//li[@class='component']"))))
   (clear "//input[@name='search']")
 
   (click "//button[contains(text(), 'Add new')]")
-  (input-text "//input[@name='name']" "wtf")
+  (input-text "//input[@name='name']" "new-component")
   (click "//button[contains(text(), 'Save')]")
-  (input-text "//input[@name='search']" "wtf")
+  (input-text "//input[@name='search']" "new-component")
   (is (= 1 (count (elements "//li[@class='component']")))))
 
