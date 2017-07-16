686c3cc0cf74ba706d90d7e7a195f91863328156
Travis CI <travis@example.com>
2017-07-16 16:46:03 +0000

    Travis-CI: update resources

--- js/compiled/out/warehouse/core.js
+++ js/compiled/out/warehouse/core.js
@@ -16,12 +16,12 @@ window.addEventListener("hashchange",(function (e){
 return secretary.core.dispatch_BANG_(location.hash);
 }));
 
-var G__16908_16911 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$initialize_DASH_db], null);
-(re_frame.core.dispatch_sync.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.dispatch_sync.cljs$core$IFn$_invoke$arity$1(G__16908_16911) : re_frame.core.dispatch_sync.call(null,G__16908_16911));
+var G__16910_16913 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$initialize_DASH_db], null);
+(re_frame.core.dispatch_sync.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.dispatch_sync.cljs$core$IFn$_invoke$arity$1(G__16910_16913) : re_frame.core.dispatch_sync.call(null,G__16910_16913));
 
-var G__16909_16912 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [warehouse.views.page], null);
-var G__16910_16913 = document.getElementById("app");
-(reagent.core.render_component.cljs$core$IFn$_invoke$arity$2 ? reagent.core.render_component.cljs$core$IFn$_invoke$arity$2(G__16909_16912,G__16910_16913) : reagent.core.render_component.call(null,G__16909_16912,G__16910_16913));
+var G__16911_16914 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [warehouse.views.page], null);
+var G__16912_16915 = document.getElementById("app");
+(reagent.core.render_component.cljs$core$IFn$_invoke$arity$2 ? reagent.core.render_component.cljs$core$IFn$_invoke$arity$2(G__16911_16914,G__16912_16915) : reagent.core.render_component.call(null,G__16911_16914,G__16912_16915));
 
 return warehouse.index.initialize();
 });
