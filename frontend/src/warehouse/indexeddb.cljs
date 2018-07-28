6bac2e66cd79c5da37afbc1b42f9f5e6e26500d0
Miloslav Nenadal <nenadalm@gmail.com>
2018-07-28 10:33:52 +0200

    [frontend]: Add indexeddb tests

--- frontend/src/warehouse/indexeddb.cljs
+++ frontend/src/warehouse/indexeddb.cljs
@@ -27,50 +27,79 @@
                        (set! (.-oncomplete tx)
                              #(go (>! ch true)))))))
 
+(defn- cursor->value-fn [select]
+  (condp = select
+    :idb/key (fn [cursor] (.-key cursor))
+    :idb/value (fn [cursor] (.-value cursor))))
+
+(defn- store-get-fn [select store]
+  (.bind
+   (condp = select
+     :idb/key (.-getKey store)
+     :idb/value (.-get store))
+   store))
+
+(defn- open-cursor-fn [select store]
+  (.bind
+   (condp = select
+     :idb/key (.-openKeyCursor store)
+     :idb/value (.-openCursor store))
+   store))
+
+(defn- count-records [source f]
+  (let [request (.count source)]
+    (set! (.-onsuccess request)
+          (fn [e] (f (.-target.result e))))))
+
+(defn- send-cursor-result [db request source]
+  (let [q (:idb/q db)
+        result (volatile! [])
+        advanced (volatile! (= 0 (:offset q 0)))
+        cursor->value (cursor->value-fn (:select q))
+        complete-result (fn [result]
+                          (count-records source
+                                         (fn [cnt] (go (>! (:idb/ch db) {:count cnt :data result})))))]
+    (set! (.-onsuccess request)
+          (fn [e]
+            (if-let [cursor (.-target.result e)]
+              (if @advanced
+                (do
+                  (vswap! result conj (cursor->value cursor))
+                  (if (= (count @result) (:limit q))
+                    (complete-result @result)
+                    (.continue cursor)))
+                (do
+                  (.advance cursor (:offset q))
+                  (vreset! advanced true)))
+              (complete-result @result))))))
+
 (defmulti where-cond (fn [db v] (first v)))
 
 (defmethod where-cond nil [db v]
-  (let [store (:idb/store db)
-        request (.count store)]
-    (set! (.-onsuccess request)
-          (fn [e]
-            (let [cnt (.-target.result e)
-                  q (:idb/q db)
-                  result (volatile! [])
-                  advanced (volatile! (= 0 (:offset q)))
-                  request (.openCursor store)]
-              (set! (.-onsuccess request)
-                    (fn [e]
-                      (if-let [cursor (.-target.result e)]
-                        (if @advanced
-                          (do
-                            (vswap! result conj (.-value cursor))
-                            (if (= (count @result) (:limit q))
-                              (go (>! (:idb/ch db) {:count cnt
-                                                    :data @result}))
-                              (.continue cursor)))
-                          (do
-                            (.advance cursor (:offset q))
-                            (vreset! advanced true)))
-                        (go (>! (:idb/ch db) {:count cnt
-                                              :data @result}))))))))))
+  (let [q (:idb/q db)
+        store (:idb/store db)
+        open-cursor (open-cursor-fn (:select q) store)
+        request (open-cursor)]
+    (send-cursor-result db request store)))
 
 (defmethod where-cond 'in [db v]
   (let [[_ _ values] v]
     (if (seq values)
-      (let [store (:idb/store db)
+      (let [q (:idb/q db)
+            store (:idb/store db)
+            store-get (store-get-fn (:select q) store)
             result (volatile! [])
             remaining (volatile! (count values))]
         (doseq [value values]
-          (let [request (.get store value)]
+          (let [request (store-get value)]
             (set! (.-onsuccess request)
                   (fn [e]
                     (when-let [res (.-target.result e)]
                       (vswap! result conj res))
                     (vswap! remaining dec)
                     (when (= 0 @remaining)
-                      (go (>! (:idb/ch db) @result))))))))
-      (go (>! (:idb/ch db) [])))))
+                      (go (>! (:idb/ch db) {:count (count @result) :data @result}))))))))
+      (go (>! (:idb/ch db) {:count 0 :data []})))))
 
 (defmethod where-cond '= [db v]
   (let [[_ x y] v
@@ -84,7 +113,7 @@
             (if-let [cursor (.-target.result e)]
               (do (vswap! result conj (.-value.id cursor))
                   (.continue cursor))
-              (go (>! (:idb/ch db) @result)))))))
+              (go (>! (:idb/ch db) {:data @result})))))))
 
 (defmethod where-cond 'and [db v]
   (let [conds (rest v)
@@ -94,10 +123,10 @@
     (doseq [cond conds]
       (where-cond (assoc db :idb/ch ch) cond))
     (a/go-loop []
-      (vswap! results conj (set (<! ch)))
+      (vswap! results conj (set (:data (<! ch))))
       (if (= n (count @results))
         (let [result (vec (apply clojure.set/intersection @results))]
-          (>! (:idb/ch db) result))
+          (>! (:idb/ch db) {:data result}))
         (recur)))))
 
 (defn query [db q]
